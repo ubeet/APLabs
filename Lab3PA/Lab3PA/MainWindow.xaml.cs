d8c0d2944d6ecd7aef0d70785a48eb6544380d76
Владислав Громов <44301460+ubeet@users.noreply.github.com>
2022-12-25 21:17:41 +0200

    Bug fixing

--- Lab3PA/Lab3PA/MainWindow.xaml.cs
+++ Lab3PA/Lab3PA/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Lab3PA.Tree;
 using static Lab3PA.Tree.AVLTree;
 
@@ -29,11 +18,20 @@ namespace Lab3PA
             InitializeComponent();
             var elGeom = new EllipseGeometry(new Point(50,50), 45, 20);
         }
+
+        private bool IsNumeric(string txt, out int num)
+        {
+            return int.TryParse(txt, out num);
+        }
         private void Btn_AddEl(object sender, RoutedEventArgs e)
         {
             var txt = textBox1.Text;
-            if (txt != "")
-                x = Insert(x, Convert.ToInt32(txt));
+            if (txt == "" || !IsNumeric(txt, out var num))
+            {
+                textBox1.Text = "";
+                return;
+            }
+            x = Insert(x, num);
             textBox1.Text = "";
             var str = PrintTree(x, 10, 10, "main");
             textBlock1.Text = str;
@@ -42,9 +40,14 @@ namespace Lab3PA
         private void Btn_RemoveEl(object sender, RoutedEventArgs e)
         {
             var txt = textBox1.Text;
-            if (txt != "")
-                x = Remove(x, Convert.ToInt32(txt));
-            
+            if (txt == "" || !IsNumeric(txt, out var num))
+            {
+                textBox1.Text = "";
+                return;
+            }
+            x = Remove(x, num);
+
+
         }
         string path = Directory
             .GetParent(Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory)
@@ -60,8 +63,12 @@ namespace Lab3PA
         private void Btn_FillRandom(object sender, RoutedEventArgs e)
         {
             var txt = textBox1.Text;
-            if (txt != "")
-                x = FillRandom(x, Convert.ToInt32(txt));
+            if (txt == "" || !IsNumeric(txt, out var num))
+            {
+                textBox1.Text = "";
+                return;
+            }
+            x = FillRandom(x, num);
             textBox1.Text = "";
             var str = PrintTree(x, 10, 10, "main");
             textBlock1.Text = str;
